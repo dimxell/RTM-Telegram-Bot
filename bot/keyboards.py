0b87e3e2f82591235cf85e5d3fcbd0780b2a425f
CreativeeBlackWolf <maksim271202@gmail.com>
2024-04-15 20:29:58 +0300

    нуы

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,9 +1,18 @@
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
-def startup_keyboard():
+def startup_keyboard() -> ReplyKeyboardMarkup:
     markup = ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True)
 
     markup.add(KeyboardButton("Поиск по релизу"))
     markup.add(KeyboardButton("Поиск по исполнителю"))
+
+    return markup
+
+
+def gen_titles_inline_keyboard(titles: list[str]) -> InlineKeyboardMarkup:
+    markup = InlineKeyboardMarkup(row_width=1)
+
+    for title in titles:
+        markup.add(InlineKeyboardButton(title, callback_data=title))
 
     return markup
--- bot/main.py
+++ bot/main.py
@@ -1,14 +1,12 @@
-import requests
-from io import BytesIO, StringIO
 from configparser import ConfigParser
 
 from telebot import asyncio_filters
 from telebot.async_telebot import AsyncTeleBot
 from telebot.asyncio_storage import StateMemoryStorage
 from telebot.asyncio_handler_backends import State, StatesGroup
-from telebot.types import Message
+from telebot.types import Message, InputMediaPhoto
 
-from bot.keyboards import startup_keyboard
+from bot.keyboards import startup_keyboard, gen_titles_inline_keyboard
 from discogsparser.parser import DiscogsAPI
 
 
@@ -31,7 +29,6 @@ async def send_on_start(message: Message):
     await bot.send_message(message.from_user.id, "Выбери что-то нахуй.", reply_markup=startup_keyboard())
 
 
-
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: message.text == "Поиск по релизу")
 async def echo_message(message: Message):
@@ -42,24 +39,15 @@ async def echo_message(message: Message):
 async def search_release(message: Message):
     releases = discogs_parser.search_release(message.text)
 
-    await bot.send_message(message.from_user.id, "\n".join(f"{r.title} ({r.release_year}) / {r.genres}" for r in releases))
+    await bot.send_media_group(message.chat.id, [InputMediaPhoto(i.cover_image_url, caption=i.title) for i in releases])
+    await bot.send_message(message.from_user.id, "Выбери релиз из списка", reply_markup=gen_titles_inline_keyboard([i.title for i in releases]))
     await bot.delete_state(message.from_user.id, message.chat.id)
 
 @bot.message_handler(func=lambda message: message.text == "testpls")
 async def test(message: Message):
-    # headers = {
-    #     'User-Agent': 'Mozilla/5.0',
-    # }
-    # r = requests.get(
-    #     "https://i.discogs.com/jh5NU8qQ7fIMan3Yi_cjEdsUczWgp6rD_RBHCn0n3Tk/rs:fit/g:sm/q:90/h:601/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE2MTc2/NzAwLTE2MTA5MDE3/MDItMTEzMS5wbmc.jpeg", 
-    #     allow_redirects=True,
-    #     headers=headers
-    # )
-
-    # photo = Image.open(StringIO(r.content))
-    # print(photo)
-    # photo.show()
-    await bot.send_photo(message.from_user.id, b'https://i.discogs.com/jh5NU8qQ7fIMan3Yi_cjEdsUczWgp6rD_RBHCn0n3Tk/rs:fit/g:sm/q:90/h:601/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE2MTc2/NzAwLTE2MTA5MDE3/MDItMTEzMS5wbmc.jpeg'.decode(), caption="god pls work")
+    await bot.send_photo(message.from_user.id, 
+                         'https://i.discogs.com/jh5NU8qQ7fIMan3Yi_cjEdsUczWgp6rD_RBHCn0n3Tk/rs:fit/g:sm/q:90/h:601/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE2MTc2/NzAwLTE2MTA5MDE3/MDItMTEzMS5wbmc.jpeg', 
+                         caption="god pls work")
 
 
 bot.add_custom_filter(asyncio_filters.StateFilter(bot))
